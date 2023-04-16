c469a31a80712b27e511effeafc4b127f93cf8b0
vincent317 <dpeng12@terpmail.edu>
2023-04-16 09:22:50 -0400

    summarization integrated

--- flask_backend/controller.py
+++ flask_backend/controller.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restx import Api, Resource, reqparse
+from flask_restx import Api, Resource, reqparse, fields
 
 import zoom_service, chatgpt_service
 from utils.webvtt_parsing import webvtt_parsing
@@ -10,6 +10,7 @@ from utils.action_item_parsing import action_item_processing
 app = Flask(__name__)
 api = Api(app, version='1.0', title='VCA Backend API',
     description='Sample APIs',)
+
 
 @api.route('/action_item_extraction/<meeting_id>')
 @api.doc(params={'meeting_id': 'Meeting ID'})
@@ -23,29 +24,29 @@ class ActionItem(Resource):
             string = '\n'.join(chunk)
             action_items_arr = None
             while True:
-                action_items_arr = action_item_service.extract_action_item(string)
+                action_items_arr = chatgpt_service.extract_action_item(string)
                 if action_items_arr is not None:
                     break
             overlapped.append(action_items_arr)
         ret = action_item_processing(overlapped)
         return ret
 
+parser = reqparse.RequestParser()
+parser.add_argument('agenda_items')
+
 @api.route('/summarization/<meeting_id>')
 @api.doc(params={'meeting_id': 'Meeting ID'})
 class Summarization(Resource):
-    def get(self, meeting_id):
-        parser = reqparse.RequestParser()
-        parser.add_argument('agenda_items')
+    @api.expect(parser)
+    def post(self, meeting_id):
         args = parser.parse_args()
         agenda_items = args['agenda_items']
 
         vtt_file_location = zoom_service.get_meeting_transcript(meeting_id)
         meeting_transcript_string = webvtt_parsing(vtt_file_location)
         
-        res = generate_summaries(agenda_items, meeting_transcript_string):
-
-
-
-
+        res = chatgpt_service.generate_summaries(agenda_items, meeting_transcript_string)
+        return {"overrall symmary" : res[0], "agenda summaries" : res[1], "additional summaries" : res[2]}
+    
 if __name__ == '__main__':
     app.run(debug=True)
