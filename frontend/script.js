a96ccab9ec855162f7a63e23bd6890a4efbfb211
yoonichoi <yooni.jy.choi@gmail.com>
2023-04-26 18:52:02 -0400

    fix on frontend - loading message to disappear after result fetched

--- frontend/script.js
+++ frontend/script.js
@@ -4,8 +4,11 @@ const resultDiv = document.getElementById('result');
 async function handleSubmit(event) {
   event.preventDefault();
 
-  // Show "loading" message
-  resultDiv.innerHTML = 'Loading meeting report...';
+  const loadingMessage = document.createElement('div');
+  loadingMessage.id = 'loading-message';
+  loadingMessage.innerText = 'Loading meeting report...';
+  resultDiv.appendChild(loadingMessage);
+  
 
   const meeting_id = document.getElementById('meeting_id').value;
   if (!meeting_id) {
@@ -37,56 +40,70 @@ async function handleSubmit(event) {
 
 
     // Create the meeting info section
-    const meetingInfoSection = document.createElement('div');
-    meetingInfoSection.innerHTML = `
-  <h2>Meeting Information:</h2><hr><br>
-  <p>Meeting Title: ${data.meeting_title}</p>
-  <p>Meeting Date: ${data.meeting_date}</p>
-  <h3>Participants:</h3>
-  <ul>
-    ${Object.entries(data.participants).map(([participant, details]) => `
-      <li>
-        <span${details.late === 'late' ? ' style="color: red;"' : ''}>${participant} (${details.late})</span>
-        <p>Speaking Time: ${details.duration}</p>
-      </li>
-    `).join('')}
-  </ul>
+    const meetingInfoSection = `
+  <div>
+    <h2>Meeting Information:</h2><hr><br>
+    <p>Meeting Title: ${data.meeting_title}</p>
+    <p>Meeting Date: ${data.meeting_date}</p>
+    <h3>Participants:</h3>
+    <ul>
+      ${Object.entries(data.participants).map(([participant, details]) => `
+        <li>
+          <span${details.late === 'late' ? ' style="color: red;"' : ''}>${participant} (${details.late})</span>
+          <p>Speaking Time: ${details.duration}</p>
+        </li>
+      `).join('')}
+    </ul>
+  </div>
 `;
-    resultDiv.appendChild(meetingInfoSection);
-
     // Create the summary section
-    const summarySection = document.createElement('div');
-    summarySection.innerHTML = `
-  <h2>Summary and Agenda:</h2><hr><br>
-  <h3>Overall summary:</h3>
-  <p>${data.summary['Overall summary']}</p>
-  <h3>Short agenda summaries:</h3>
-  <ul>
-    ${Object.entries(data.summary['Short agenda summaries']).map(([agenda, summary]) => `
-      <li>
-        <h4>${agenda}</h4>
-        <p>${summary}</p>
-        <button onclick="toggleLongSummary('${agenda}')">Show/hide long summary</button>
-        <div id="${agenda}_long_summary" style="display: none;">
-          <p>${data.summary['Detailed agenda summaries'][agenda]}</p>
-        </div>
-      </li>
-    `).join('')}
-  </ul>
-  <h3>Additional summaries:</h3>
-  <p>${data.summary['Additional summaries']}</p>
+    const summarySection = `
+  <div>
+    <h2>Summary and Agenda:</h2><hr><br>
+    <h3>Overall summary:</h3>
+    <p>${data.summary['Overall summary']}</p>
+    <h3>Short agenda summaries:</h3>
+    <ul>
+      ${Object.entries(data.summary['Short agenda summaries']).map(([agenda, summary]) => `
+        <li>
+          <h4>${agenda}</h4>
+          <p>${summary}</p>
+          <button onclick="toggleLongSummary('${agenda}')">Show/hide long summary</button>
+          <div id="${agenda}_long_summary" style="display: none;">
+            <p>${data.summary['Detailed agenda summaries'][agenda]}</p>
+          </div>
+        </li>
+      `).join('')}
+    </ul>
+    <h3>Additional summaries:</h3>
+    <p>${data.summary['Additional summaries']}</p>
+  </div>
 `;
-    resultDiv.appendChild(summarySection);
 
     // Create the action items section
-    const actionItemsSection = document.createElement('div');
-    actionItemsSection.innerHTML = `
-  <h2>Action Items:</h2><hr><br>
-  <ul>
-    ${data.action_items.map(item => `<li>${item}</li>`).join('')}
-  </ul>
+    const actionItemsSection = `
+  <div>
+    <h2>Action Items:</h2><hr><br>
+    <ul>
+      ${data.action_items.map(item => `<li>${item}</li>`).join('')}
+    </ul>
+  </div>
 `;
-    resultDiv.appendChild(actionItemsSection);
+  
+  resultDiv.innerHTML = `
+    ${meetingInfoSection}
+    ${summarySection}
+    ${actionItemsSection}
+  `;
+
+    // // Create the download button
+    // const downloadButton = document.createElement('button');
+    // downloadButton.innerText = 'Download Generated Report';
+    // downloadButton.onclick = downloadReport;
+
+    // resultDiv.appendChild(downloadButton);
+    loadingMessage.remove()
+  
 
   } catch (error) {
     const errorSection = document.createElement('div');
@@ -102,4 +119,14 @@ async function handleSubmit(event) {
 function toggleLongSummary(agenda) {
   const longSummary = document.getElementById(`${agenda}_long_summary`);
   longSummary.style.display = longSummary.style.display === 'none' ? 'block' : 'none';
+}
+
+function downloadReport() {
+  const reportHTML = resultDiv.innerHTML;
+  const reportBlob = new Blob([reportHTML], {type: 'text/html'});
+  const reportURL = URL.createObjectURL(reportBlob);
+  const downloadLink = document.createElement('a');
+  downloadLink.href = reportURL;
+  downloadLink.download = 'meeting_report.html';
+  downloadLink.click();
 }
